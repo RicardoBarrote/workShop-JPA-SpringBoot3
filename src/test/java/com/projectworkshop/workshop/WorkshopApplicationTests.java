1e545a595299c77ef479aa49091ab1c9debf72d0
RicardoBarrote <ricardoambscfc@gmail.com>
2024-06-24 18:46:05 -0300

    Project Created
    Signed-off-by: RicardoBarrote <ricardoambscfc@gmail.com>

---
+++ src/test/java/com/projectworkshop/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectworkshop.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
