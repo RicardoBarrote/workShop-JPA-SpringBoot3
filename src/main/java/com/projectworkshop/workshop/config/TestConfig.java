0f82ec03272a53f62cd6783967e84619742c674c
RicardoBarrote <ricardoambscfc@gmail.com>
2024-06-26 16:54:47 -0300

    OrderStatus class of enumerated type
    Signed-off-by: RicardoBarrote <ricardoambscfc@gmail.com>

--- src/main/java/com/projectworkshop/workshop/config/TestConfig.java
+++ src/main/java/com/projectworkshop/workshop/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projectworkshop.workshop.entities.Order;
 import com.projectworkshop.workshop.entities.User;
+import com.projectworkshop.workshop.entities.enums.OrderStatus;
 import com.projectworkshop.workshop.repositories.OrderRepository;
 import com.projectworkshop.workshop.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Roberto Marinho", "Roberto@gmail.com", "99999999999", "12345");
 		User user2 = new User(null, "Marina Lima", "Marina@gmail.com", "99999999999", "12345");
 
-		Order order1 = new Order(null, Instant.parse("2024-06-20T19:01:07Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2024-08-17T20:40:25Z"), user2);
-		Order order3 = new Order(null, Instant.parse("2024-03-19T18:37:29Z"), user1);
+		Order order1 = new Order(null, Instant.parse("2024-06-20T19:01:07Z"), OrderStatus.PAID, user1);
+		Order order2 = new Order(null, Instant.parse("2024-08-17T20:40:25Z"), OrderStatus.WAITING_PAYMENT, user2);
+		Order order3 = new Order(null, Instant.parse("2024-03-19T18:37:29Z"), OrderStatus.PAID, user1);
 
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
